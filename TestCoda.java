91e8ec51d77a90dc89f43f840ffbbb342f613477
TommasiLarkingDavide <davide.tommasilarking@marconibari.edu.it>
2026-05-04 16:21:33 +0000

    ConsegnaD

--- TestCoda.java
+++ TestCoda.java
@@ -1,1 +1,10 @@
 //Implementare qui la classe TestCoda, per l'esercizio del gioco della patata bollente
+public class Coda<T> {
+    private Nodo<T> head;
+    private Nodo<T> tail;
+
+    public Coda() {
+        this.head = null;
+        this.tail = null;
+    }
+}
